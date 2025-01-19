d5c539f2a3a5eb5878bcedb29bccd5a9c67236ff
Carsten Paasch <carsten.paasch@materna.group>
2025-01-19 20:14:58 +0100

    Changes to try to deploy to github

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
-  base: "/thai-abc-adventure",
+  //base: "/thai-abc-adventure",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
