91b620f6ea3fca2d6bc9d9b8e7141cbe6ce191de
Carsten Paasch <carsten.paasch@materna.group>
2025-01-19 22:54:37 +0100

    Fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Add the log statement inside the function block
-  console.log(`Vite is running in ${mode} mode`);
-
-  return {
-    server: {
-      host: "::",
-      port: 8080,
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/thai-abc-adventure/" : "/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [react()],
-    base: mode === "production" ? "/thai-abc-adventure/" : "/",
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-  };
-});
+  },
+}));
